e275c1f2e98083e2c230c822be246c266cfe683b
David Campey <campey@informationlogistics.co.za>
2013-08-29 14:48:08 -0400

    initial commit

---
+++ techtourism/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techtourism::Application.config.secret_token = '9234944c54527b9aeb21f953d6c96dc3473f307e41721732e343734d7cc2451b5103426821ffec10d7020882805596b4f1423c53236ec7541d32aff8c7209363'
